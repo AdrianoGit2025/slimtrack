a1f3d62669b618e8b3c51b602142eb3222ceaa6e
AdrianoGit2025 <adrianofreitas2017@gmail.com>
2025-07-17 15:03:38 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+import database
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+if __name__ == "__main__":
+    database.init_db()
+    app.run(debug=True)
